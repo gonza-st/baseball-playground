a2560658b627ed77b60e8a1019eb2a4030cab02f
Heonbyeong <abh0920one@gmail.com>
2024-12-05 12:56:29 +0900

    chore: init

---
+++ heon-playground/baseball-playground/src/test/kotlin/com/gonza/heon/baseballplayground/BaseballPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gonza.heon.baseballplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaseballPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
