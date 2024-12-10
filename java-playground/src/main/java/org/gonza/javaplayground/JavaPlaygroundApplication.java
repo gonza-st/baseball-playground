b7e6a3630881dd53dcb0f5ef0824ef7982bb80ed
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-12-10 10:18:16 +0900

    add: 게임 진행하는 main class 추가
    게임 진행하는 main class 추가

--- java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
+++ java-playground/src/main/java/org/gonza/javaplayground/JavaPlaygroundApplication.java
@@ -1,13 +1,34 @@
 package org.gonza.javaplayground;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaPlaygroundApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaPlaygroundApplication.class, args);
+		// SpringApplication.run(JavaPlaygroundApplication.class, args);
+		RandomNumberGenerator numberGenerator = new RandomNumberGenerator();
+		BaseballGame baseballGame = new BaseballGame(numberGenerator);
+
+		int isContinue = 1;
+		while (isContinue == 1) {
+			baseballGame.init();
+			baseballGameRun(baseballGame);
+			isContinue = OutputView.gameResultView();
+		}
 	}
 
+	private static void baseballGameRun(BaseballGame baseballGame) {
+		String retryNumber = InputView.inputNumber();
+		BaseballGameNumber userNumber = new BaseballGameNumber(retryNumber);
+		int strikeCount = baseballGame.getStrikeCount(userNumber);
+
+		if (strikeCount == 3) {
+			return;
+		}
+
+		int ballCount = baseballGame.getBallCount(userNumber);
+		OutputView.strikeBallView(strikeCount, ballCount);
+		baseballGameRun(baseballGame);
+	}
 }
