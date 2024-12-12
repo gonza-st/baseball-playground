535ad5821d395ae7b965ff0e914ef13a3bda4b08
leeMK09 <dlaudrb09@naver.com>
2024-12-12 13:09:39 +0900

    test: 해당 값이 스트라이크가 아닌지 테스트 코드 추가

--- java-playground/src/test/java/org/gonza/javaplayground/domain/StrikeTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/domain/StrikeTest.java
@@ -11,13 +11,21 @@ class StrikeTest {
     void 해당_숫자가_스트라이크라면_결과는_참이다() {
         int number = 123;
         int answer = 123;
-        int wrong = 999;
         Strike strike = new Strike();
 
         boolean isStrike = strike.correct(answer, number);
-        boolean isWrong = strike.correct(wrong, number);
 
         assertThat(isStrike).isTrue();
+    }
+
+    @Test
+    void 해당_숫자가_스트라이크가_아니라면_결과는_거짓이다() {
+        int number = 123;
+        int wrong = 999;
+        Strike strike = new Strike();
+
+        boolean isWrong = strike.correct(wrong, number);
+
         assertThat(isWrong).isFalse();
     }
 }
