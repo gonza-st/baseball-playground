29005f8a4c4ef165ef537855a1488707537bad6f
Heon <abh0920one@gmail.com>
2024-12-26 00:01:02 +0900

    feat: inputview print 제거

--- heon-playground/baseball-playground/src/main/kotlin/com/gonza/heon/baseballplayground/InputView.kt
+++ heon-playground/baseball-playground/src/main/kotlin/com/gonza/heon/baseballplayground/InputView.kt
@@ -4,13 +4,11 @@ class InputView private constructor() {
 
 
     fun answer(): String {
-        print(INPUT_MESSAGE)
         val number = readlnOrNull().orEmpty()
         return number
     }
 
     companion object {
-        private const val INPUT_MESSAGE = "숫자를 입력해 주세요 : "
         private var instance: InputView? = null
 
         fun getInstance(): InputView {
