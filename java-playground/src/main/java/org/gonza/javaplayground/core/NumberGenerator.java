2b554b8bb397e009137bef4089e397c9e82a2275
hongxeob <sjun2918@naver.com>
2024-12-26 14:18:52 +0900

    리뷰 반영 : Random 생성자 초기화

--- java-playground/src/main/java/org/gonza/javaplayground/core/NumberGenerator.java
+++ java-playground/src/main/java/org/gonza/javaplayground/core/NumberGenerator.java
@@ -8,6 +8,12 @@ public class NumberGenerator {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
 
+    private final Random random;
+
+    public NumberGenerator() {
+        this.random = new Random();
+    }
+
     public Numbers generateRandomNumber(int size) {
         Validator.validateListSize(size);
         return new Numbers(generateUniqueNumbers(size));
@@ -15,7 +21,6 @@ public class NumberGenerator {
 
     private List<Integer> generateUniqueNumbers(int size) {
         Set<Integer> numberSet = new HashSet<>();
-        Random random = new Random();
 
         while (numberSet.size() < size) {
             numberSet.add(random.nextInt(MAX_NUMBER) + MIN_NUMBER);
