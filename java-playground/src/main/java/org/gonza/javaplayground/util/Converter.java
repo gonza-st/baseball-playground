a43688ad9b203d16bdb5b8d71783256032a6a463
hongxeob <sjun2918@naver.com>
2024-12-28 17:31:46 +0900

    리뷰 반영 : Converter 검증 추가

--- java-playground/src/main/java/org/gonza/javaplayground/util/Converter.java
+++ java-playground/src/main/java/org/gonza/javaplayground/util/Converter.java
@@ -6,6 +6,12 @@ import java.util.stream.Collectors;
 public class Converter {
 
     public static List<Integer> convertStringToNumberList(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+
+        Validator.validateNumeric(input);
+
         return input.chars()
                 .map(Character::getNumericValue)
                 .boxed()
