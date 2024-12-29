8116c70fa4c982f51eae58da6410eff8a1eab9b0
leemk09 <dlaudrb09@naver.com>
2024-12-29 20:25:41 +0900

    test: 규칙 검증 객체에 대한 자릿수 테스트

--- java-playground/src/main/java/org/gonza/javaplayground/domain/RuleValidator.java
+++ java-playground/src/main/java/org/gonza/javaplayground/domain/RuleValidator.java
@@ -18,8 +18,8 @@ public class RuleValidator {
     }
 
     public void validateMaxSize(List<Integer> number) {
-        int maxSize = rule.getMaxSize();
-        if (number.size() != maxSize) {
+        int inputSize = number.size();
+        if (rule.isOverMaxSize(inputSize) || rule.isBelowMaxSize(inputSize)) {
             throw new InvalidNumberLengthException("입력한 값이 3자리수가 아닙니다.");
         }
     }
--- java-playground/src/test/java/org/gonza/javaplayground/domain/RuleValidatorTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/domain/RuleValidatorTest.java
@@ -1,10 +1,11 @@
 package org.gonza.javaplayground.domain;
 
+import org.gonza.javaplayground.exception.InvalidLengthException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class RuleValidatorTest {
 
@@ -12,5 +13,11 @@ class RuleValidatorTest {
     void 입력한_값과_규칙의_자릿수가_다르면_예외가_발생합니다() {
         List<Integer> answer = List.of(1, 2, 3);
         List<Integer> number = List.of(1, 2);
+        Rule rule = new Rule();
+        RuleValidator ruleValidator = new RuleValidator(rule);
+
+        assertThatThrownBy(() -> ruleValidator.validateLength(answer, number))
+                .isInstanceOf(InvalidLengthException.class)
+                .hasMessage("입력값과 정답의 자릿수가 일치하지 않습니다");
     }
 }
